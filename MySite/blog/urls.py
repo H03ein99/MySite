9c9eb7e2586c04bb89f1729ee62036ca6870ff9c
Mohammad Hossein Ghorbani <hosseingh539@gmail.com>
2025-02-24 03:44:07 -0800

    Update blog app and templates
    added view for blog-home and blog-single
    urls configured so we can access blog posts by id
    template statics set
    added url tags for blog-home
    deleted blog-single from base now we can access posts only through blog-home

--- MySite/blog/urls.py
+++ MySite/blog/urls.py
@@ -1,5 +1,7 @@
 from blog import views
+from django.urls import path
 
 urlpatterns = [
-    
+    path('', views.home, name='blog-home'),
+    path('<int:id>', views.single, name='blog-single'),
 ]

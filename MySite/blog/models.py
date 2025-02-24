0a131c9dccf7a8ad0a7b16dd965b076df6614124
Mohammad Hossein Ghorbani <hosseingh539@gmail.com>
2025-02-24 05:42:35 -0800

    Update models in blog and website
    added post model in blog and contact model in website

--- MySite/blog/models.py
+++ MySite/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class Post(models.Model):
+        author =models.ForeignKey(
+            settings.AUTH_USER_MODEL,
+            on_delete=model.CASCADE,
+        )
+        image = models.image_field(upload_to='uploads/% Y/% m/% d/')
+        title = models.CharField(max_length=100)
+        content = models.TextField()
+        # category 
+        # tag
+        counted_view = models.PositiveIntegerField(default=0)
+        status = models.BooleanField(default=False)
+        published_date = models.DateField(null=True)
+        created_date = models.DateField(auto_now_add=True)
+        updated_date = models.DateField(auto_add=True)
+
--- MySite/website/models.py
+++ MySite/website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField( max_length=255)
+    subject = models.CharField( max_length=255)
+    email = models.EmailField()
+    text = models.TextField()
+    created_date =models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)

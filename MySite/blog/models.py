885ea96b041af6cad1728c5226b922c89b86b0ab
Mohammad Hossein Ghorbani <hosseingh539@gmail.com>
2025-02-24 06:13:04 -0800

    Update models and admins

--- MySite/blog/models.py
+++ MySite/blog/models.py
@@ -4,9 +4,10 @@ from django.conf import settings
 class Post(models.Model):
         author =models.ForeignKey(
             settings.AUTH_USER_MODEL,
-            on_delete=model.CASCADE,
+            on_delete=models.CASCADE,
+            default = -1
         )
-        image = models.image_field(upload_to='uploads/% Y/% m/% d/')
+        image = models.ImageField(upload_to='uploads/')
         title = models.CharField(max_length=100)
         content = models.TextField()
         # category 
@@ -15,8 +16,8 @@ class Post(models.Model):
         status = models.BooleanField(default=False)
         published_date = models.DateField(null=True)
         created_date = models.DateField(auto_now_add=True)
-        updated_date = models.DateField(auto_add=True)
+        updated_date = models.DateField(auto_now=True)
 
-    def __str__(self):
-        return f"{self.title} - {self.author}"
+        def __str__(self):
+            return f"{self.title}"
     

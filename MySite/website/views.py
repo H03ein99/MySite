7178cdc7bb8c4323953978709602b6b1475895d9
Mohammad Hossein Ghorbani <hosseingh539@gmail.com>
2025-02-23 23:32:40 -0800

    update temps

--- MySite/website/views.py
+++ MySite/website/views.py
@@ -5,4 +5,7 @@ def index(request):
     return render(request, 'website/index.html')
 
 def about(request):
-    return render(request, 'website/about.html')    
+    return render(request, 'website/about.html') 
+
+def contact(request):
+    return render(request, 'website/contact.html')       

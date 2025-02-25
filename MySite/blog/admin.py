e9947e594fa8eba348b71b7aed806b8e11c5cd19
Mohammad Hossein Ghorbani <hosseingh539@gmail.com>
2025-02-25 09:14:21 -0800

    Update view
    posts that their published dates are in future wont be shown anymore

--- MySite/blog/admin.py
+++ MySite/blog/admin.py
@@ -3,7 +3,7 @@ from blog.models import Post
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = "created_date"
-    list_display = ['author', 'title', 'counted_view', 'created_date', 'published_date', ]
+    list_display = ['author', 'title', 'counted_view', 'created_date', 'published_date','id' ]
 
 admin.site.register(Post, PostAdmin)
  
--- MySite/blog/views.py
+++ MySite/blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.utils import timezone
 
 # Create your views here.
+today = timezone.now().date()
 def home(request):
-    posts = Post.objects.filter(status=1)
-    popular_posts = Post.objects.all().filter(status=1).order_by('-counted_view')
+    
+    posts = Post.objects.filter(status=1, published_date__lte=today).order_by('-published_date')
+    popular_posts = Post.objects.all().filter(status=1).order_by('-counted_view')[:3]
     context = {
         'posts' : posts,
         'popular_posts' :popular_posts,
@@ -12,7 +15,7 @@ def home(request):
     return render(request, 'blog/blog-home.html', context)
 
 def single(request, id):
-    post = get_object_or_404(Post, id=id, status=1)
+    post = get_object_or_404(Post, id=id, status=1, published_date__lte=today)
     popular_posts = Post.objects.all().filter(status=1).order_by('-counted_view')
     previous = post.previous_post()
     next = post.next_post()

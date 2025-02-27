7aa68d18b4c09dd298e422197dbf419cc35a9773
Mohammad Hossein Ghorbani <hosseingh539@gmail.com>
2025-02-27 04:23:02 -0800

    Update blog
    add cattegories template tag to blog-home

--- MySite/blog/templatetags/blog_tags.py
+++ MySite/blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post, Category
 from django.utils import timezone
 
 today = timezone.now()
@@ -23,4 +23,19 @@ def recentposts():
         
     }
 
+
+@register.inclusion_tag("blog/blog-cats.html")
+def blogcats():
+    posts = Post.objects.filter(status=1, published_date__lte=today)
+    categories = Category.objects.all()
+    cat_counts = {}
+    for name in categories:
+        count =posts.filter(category = name).count()
+        if count>0:
+            cat_counts[name] = count
+    cats = {k: v for k, v in sorted(cat_counts.items(), key=lambda item: item[1], reverse=True)}    
+    return {
+        
+        'categories': cats,
+    }
 

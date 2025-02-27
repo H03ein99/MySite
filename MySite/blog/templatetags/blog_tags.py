f9eafb068e1de9831230c5937b6dc3540ba8328d
Mohammad Hossein Ghorbani <hosseingh539@gmail.com>
2025-02-26 23:27:08 -0800

    Update blog
    in blog single and blog home we show popular posts using inclusion tag

--- MySite/blog/templatetags/blog_tags.py
+++ MySite/blog/templatetags/blog_tags.py
@@ -1,3 +1,15 @@
 from django import template
+from blog.models import Post
+from django.utils import timezone
+
 
 register = template.Library()
+
+@register.inclusion_tag("blog/popularposts.html")
+def popularposts():
+    posts = Post.objects.filter(status=1).order_by('-counted_view')[:3]
+    
+    return {
+        'posts': posts
+        
+    }

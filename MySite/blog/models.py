38111e5da7f4f194c8c044252034dafe77e12b6f
Mohammad Hossein Ghorbani <hosseingh539@gmail.com>
2025-02-27 04:34:44 -0800

    Update post models
    changed created published and updated time filed fro datefield to datetimefield

--- MySite/blog/models.py
+++ MySite/blog/models.py
@@ -27,9 +27,9 @@ class Post(models.Model):
         # tag
         counted_view = models.PositiveIntegerField(default=0)
         status = models.BooleanField(default=False)
-        published_date = models.DateField(null=True)
-        created_date = models.DateField(auto_now_add=True)
-        updated_date = models.DateField(auto_now=True)
+        published_date = models.DateTimeField(null=True)
+        created_date = models.DateTimeField(auto_now_add=True)
+        updated_date = models.DateTimeField(auto_now=True)
         class Meta:
             ordering = ["-created_date"]
         def __str__(self):

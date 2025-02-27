7d15154829423d79be0a07eacf4cf5b8ebba5ae8
Mohammad Hossein Ghorbani <hosseingh539@gmail.com>
2025-02-26 23:43:47 -0800

    Update blog
    added recent posts to single and home using inclusion tag

--- MySite/blog/templatetags/blog_tags.py
+++ MySite/blog/templatetags/blog_tags.py
@@ -2,7 +2,7 @@ from django import template
 from blog.models import Post
 from django.utils import timezone
 
-
+today = timezone.now()
 register = template.Library()
 
 @register.inclusion_tag("blog/popularposts.html")
@@ -10,6 +10,17 @@ def popularposts():
     posts = Post.objects.filter(status=1).order_by('-counted_view')[:3]
     
     return {
-        'posts': posts
+        'posts': posts,
         
     }
+
+@register.inclusion_tag("blog/recentposts.html")
+def recentposts():
+    posts = Post.objects.filter(status=1, published_date__lte=today).order_by('-published_date')[:3]
+    
+    return {
+        'posts': posts,
+        
+    }
+
+

7b1a5ea1c0b8681411eb88878c309102d676d309
Mohammad Hossein Ghorbani <hosseingh539@gmail.com>
2025-02-27 03:51:12 -0800

    Update blog
    modified template names

--- MySite/blog/templatetags/blog_tags.py
+++ MySite/blog/templatetags/blog_tags.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 today = timezone.now()
 register = template.Library()
 
-@register.inclusion_tag("blog/popularposts.html")
+@register.inclusion_tag("blog/blog-popular-posts.html")
 def popularposts():
     posts = Post.objects.filter(status=1).order_by('-counted_view')[:3]
     
@@ -14,7 +14,7 @@ def popularposts():
         
     }
 
-@register.inclusion_tag("blog/recentposts.html")
+@register.inclusion_tag("blog/blog-recent-posts.html")
 def recentposts():
     posts = Post.objects.filter(status=1, published_date__lte=today).order_by('-published_date')[:3]
     

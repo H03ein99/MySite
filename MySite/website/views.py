6fbc7003bfcbd8500c543ba70ee848303433cd1c
Mohammad Hossein Ghorbani <hosseingh539@gmail.com>
2025-02-28 03:32:06 -0800

    Update web-index
    Add recent blog posts to index

--- MySite/website/views.py
+++ MySite/website/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
+from blog.models import Post, Category
+from django.utils import timezone
 # Create your views here.
+today = timezone.now()
 def index(request):
-    return render(request, 'website/index.html')
+    posts = Post.objects.filter(status=1, published_date__lte=today).order_by('-published_date')[:6]
+    context = {
+        'posts': posts
+    }
+    return render(request, 'website/index.html', context)
 
 def about(request):
     return render(request, 'website/about.html') 

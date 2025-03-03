dc84cc4cfafef24ca2e5fec298912e5caef541d8
Mohammad Hossein Ghorbani <hosseingh539@gmail.com>
2025-03-03 14:21:12 -0800

    Update urls.py

--- MySite/accounts/urls.py
+++ MySite/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
+from accounts import views
 app_name = "accounts"
 
 urlpatterns = [
-    
+    path('login/', login_view, name="login"), # login
+    # path('logout/', logout_view, name="logout"),
+    path('signup/', signup_view, name="signup"), #signup
 ]

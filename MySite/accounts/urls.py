39737acc6288334c67d1335ac5c7dab4a810532e
Mohammad Hossein Ghorbani <hosseingh539@gmail.com>
2025-03-04 05:38:22 -0800

    Update urls.py
    Import and add needed urls

--- MySite/accounts/urls.py
+++ MySite/accounts/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from accounts.views import *
+from django.contrib.auth import views as auth_views
 app_name = "accounts"
 
 urlpatterns = [
     path('login/', login_view, name="login"), # login
     path('logout/', logout_view, name="logout"), #logout
     path('signup/', signup_view, name="signup"), #signup
+    # reset and forget password
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name="password_reset"), # password reset
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"), # password reset
+    path('reset/done', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete")
 ]
